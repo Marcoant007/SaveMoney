1fefe41b941d5cadb0da7270fcdbafadb1822a82
Marcoant007 <marcoantnovo@gmail.com>
2022-07-04 11:25:05 -0300

    finalizando crud

--- src/main/java/com/example/savemoney/controllers/UserController.java
+++ src/main/java/com/example/savemoney/controllers/UserController.java
@@ -3,13 +3,17 @@ package com.example.savemoney.controllers;
 import java.net.URI;
 import java.security.Provider.Service;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,12 +41,30 @@ public class UserController {
         Page<UserDTO> userList = userService.listAllUsers(pageRequest);
         return ResponseEntity.ok().body(userList);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findUserById(Long id){
+        UserDTO userDTO = userService.findUserById(id);
+        return ResponseEntity.ok().body(userDTO);
+    }
     
     @PostMapping()
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
         userDTO = userService.createUser(userDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userDTO.getId()).toUri();
         return ResponseEntity.created(uri).body(userDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> updatedUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        userDTO = userService.updatedUser(id, userDTO);
+        return ResponseEntity.ok().body(userDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleted(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/example/savemoney/services/UserService.java
+++ src/main/java/com/example/savemoney/services/UserService.java
@@ -1,10 +1,12 @@
 package com.example.savemoney.services;
 
 import java.util.List;
+import java.util.Optional;
 
-import javax.management.ServiceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.savemoney.dtos.UserDTO;
 import com.example.savemoney.models.users.User;
 import com.example.savemoney.repositories.UserRepository;
+import com.example.savemoney.services.exceptions.DatabaseException;
+import com.example.savemoney.services.exceptions.ServiceNotFoundException;
 
 @Service
 public class UserService {
@@ -33,6 +37,35 @@ public class UserService {
         Page<User> usersList = userRepository.findAll(pageRequest);
         Page<UserDTO> userDTO = usersList.map(userObject -> new UserDTO(userObject));
         return userDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findUserById(Long id){
+        Optional<User> userOp = userRepository.findById(id);
+        User userEntity = userOp.orElseThrow(() -> new ServiceNotFoundException("Entity User Not Found"));
+        return new UserDTO(userEntity);
+    }
+
+    @Transactional
+    public UserDTO updatedUser(Long id, UserDTO userDTO){
+        try {
+            User userEntity = userRepository.getOne(id);
+            transformDTOInEntity(userDTO, userEntity);
+            userEntity = userRepository.save(userEntity);
+            return new UserDTO(userEntity);
+        } catch (Exception e) {
+            throw new ServiceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id){
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ServiceNotFoundException("Id not found " + id);
+        } catch(DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity violation");
+        }
     }
 
 
